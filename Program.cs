eedb52306f82126c6d545e0acddc70ea05225ac3
Nathan Center <nathanjcenter@gmail.com>
2022-06-01 09:48:13 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace leetcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums={1,2,3,4};
+            
+            for (int i=1;i<nums.Length; ++i){
+                nums[i]+=nums[i-1];
+                
+            }
+            
+        }
+    }
+}
