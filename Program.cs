46949df0c80831ea91ac1fcc58e40350be6b785d
Nathan Center <nathanjcenter@gmail.com>
2022-06-02 09:52:22 -0500

    added leet code practice

--- Program.cs
+++ Program.cs
@@ -1,17 +1,32 @@
 ﻿using System;
-
+using System.Linq;
 namespace leetcode
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int[] nums={1,2,3,4};
+            //int[] nums={1,2,3,4};
             
-            for (int i=1;i<nums.Length; ++i){
-                nums[i]+=nums[i-1];
+            //for (int i=1;i<nums.Length; ++i){
+               // nums[i]+=nums[i-1];
                 
-            }
+            //}
+            
+            //136. Single Number
+            /*
+            Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+            */
+            int [] nums={2,2,1};
+            int result = 0;
+        for(int i = 0; i < nums.Length; i++){
+            result ^= nums[i];
+        }
+        Console.WriteLine(result);
+
+
             
         }
     }
