77b499a05736eb9b9cd90895788c1330b808d86e
Nathan Center <nathanjcenter@gmail.com>
2022-06-07 08:33:29 -0500

    daily leetcode practice

--- Program.cs
+++ Program.cs
@@ -79,24 +79,18 @@ You must solve the problem without using any built-in library for handling large
             }*/
 
             /*
-            You are given a phone number as a string number. number consists of digits, spaces ' ', and/or dashes '-'.
+            Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.*/
+            int [] numbers={1,2,3};
+            if (numbers.Length !=numbers.Distinct().Count()){
+               Console.WriteLine("True");
+            }
+            else{
+                Console.WriteLine("False");
+            }
+         
 
-You would like to reformat the phone number in a certain manner. Firstly, remove all spaces and dashes. Then, group the digits from left to right into blocks of length 3 until there are 4 or fewer digits. The final digits are then grouped as follows:
 
-    2 digits: A single block of length 2.
-    3 digits: A single block of length 3.
-    4 digits: Two blocks of length 2 each.
-
-The blocks are then joined by dashes. Notice that the reformatting process should never produce any blocks of length 1 and produce at most two blocks of length 2.*/
-
-
-string number="1-23-45 6";
-string trim=number.Replace(" ","").Replace("-","");
-string formant=trim.Insert(3,"-");
-Console.WriteLine(formant);
-
-        
     }
-    }
+}
 }
 
