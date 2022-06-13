d10ee058aa8d5edcad56d27e69a1c245ff9c5314
Nathan Center <nathanjcenter@gmail.com>
2022-06-13 09:54:42 -0500

    daily leetcode practice

--- Program.cs
+++ Program.cs
@@ -163,6 +163,7 @@ Since the return type is an integer, the decimal digits are truncated, and only 
 
 */
   
+  /*
 int x=8;
 double answer=Math.Sqrt(x);
 
@@ -171,7 +172,29 @@ double roundUp=Math.Ceiling(answer);
 Console.WriteLine(roundUp);
 
     }
+    */
 
+    /*We define the usage of capitals in a word to be right when one of the following cases holds:
+
+    All letters in this word are capitals, like "USA".
+    All letters in this word are not capitals, like "leetcode".
+    Only the first letter in this word is capital, like "Google".
+
+Given a string word, return true if the usage of capitals in it is right.*/
+
+string word="usA";
+
+char [] charArry=word.ToCharArray();
+
+foreach(char convert in charArry){
+
+bool b=Char.IsUpper(convert);
+   Console.WriteLine(b);
+}
+   
+    
+
+}
 }
 }
 
